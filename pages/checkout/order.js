12efddb3f20842e3e469b61d9c5be828cb0ab2e3
bestfizzz <minhhoangnguyen19122004@gmail.com>
2023-05-16 22:26:31 +0700

    Update order.js

--- pages/checkout/order.js
+++ pages/checkout/order.js
@@ -9,14 +9,14 @@ export default function CheckOut() {
     return (
         <Layout>
             <div className="mt-2 w-fit">
-                <a href="/" className="text-black" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                <Link href="/" className="text-black" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 inline-flex">
                         <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
                     </svg>
                     <span  className={`inline-flex overflow-hidden whitespace-nowrap transition-transform duration-500 ${isHovered ? "translate-x-0" : "-translate-x-1/2 text-transparent"}`}>
                         Return to homepage
                     </span>
-                </a>
+                </Link>
             </div>
             <div className='flex items-start'>
                 <Breadcrumbs separator=">" className='bg-transparent w-full self-start pl-0 md:pl-6'>
