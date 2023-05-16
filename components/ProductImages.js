08b9a5b9a62e10788611f1c47c603533faf546bd
bestfizzz <minhhoangnguyen19122004@gmail.com>
2023-05-16 23:03:04 +0700

    Update ProductImages.js

--- components/ProductImages.js
+++ components/ProductImages.js
@@ -3,9 +3,10 @@ import { Carousel } from "@material-tailwind/react";
 export default function ProductImages({ images }) {
     return (
         <Carousel className="rounded-xl max-w-[300px] max-h-[300px]">
-            {images?.map(image => {
+            {images?.map((image,index) => {
                 return (
                     <img
+                        key={index}
                         src={image}
                         alt="image 1"
                         className="h-full w-full object-cover"
